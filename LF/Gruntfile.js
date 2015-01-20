c54c0c064c66307a86c9325d2cbf89332e343466
perry- <alexander.perry@ciber.com>
2015-01-20 18:05:20 +0100

    Forgot to add files..oops @channel

---
+++ LF/Gruntfile.js
@@ -0,0 +1,33 @@
+/* global module:false */
+module.exports = function(grunt) {
+	var port = grunt.option('port') || 8000;
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		connect: {
+			server: {
+				options: {
+					port: port,
+					base: '.'
+				}
+			}
+		},
+
+		watch: {
+			main: {
+				files: [ 'Start/**.*' ],
+				tasks: 'default'
+			}
+		}
+
+	});
+
+	// Dependencies
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+
+	// Default task
+	grunt.registerTask( 'default', [ 'connect', 'watch' ] );
+
+};
